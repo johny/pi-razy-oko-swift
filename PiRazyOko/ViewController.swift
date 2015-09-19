d5b103f1d5bdb822a3e9d3b4e1c9a5005fc8b6f8
Jan Kraus <kraus.jan@gmail.com>
2015-09-19 15:04:23 +0100

    Initial Commit

---
+++ PiRazyOko/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PiRazyOko
+//
+//  Created by Jan Kraus on 19/09/15.
+//  Copyright © 2015 Jan Kraus Stanko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
