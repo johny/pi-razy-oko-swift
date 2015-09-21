b1c8ec9949de0ed2e29ea55efa283dd7823313ef
Jan Kraus <kraus.jan@gmail.com>
2015-09-21 14:12:43 +0100

    Finishing tweaks, adds description as HTML

--- PiRazyOko/AboutViewController.swift
+++ PiRazyOko/AboutViewController.swift
@@ -9,31 +9,44 @@
 import UIKit
 
 class AboutViewController: UIViewController {
+  
+  @IBOutlet weak var webView: UIWebView!
+  
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    if let htmlFile = NSBundle.mainBundle().pathForResource("PiRazyOko", ofType: "html") {
 
-        // Do any additional setup after loading the view.
+      if let htmlData = NSData(contentsOfFile: htmlFile) {
+        let baseUrl = NSURL.fileURLWithPath(NSBundle.mainBundle().bundlePath)
+        webView.loadData(htmlData, MIMEType: "text/html", textEncodingName: "UTF-8", baseURL: baseUrl)
+      }
+    
     }
+    
+    
+    
+  }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
   
   
   @IBAction func closeView() {
     dismissViewControllerAnimated(true, completion: nil)
   }
 
-    /*
-    // MARK: - Navigation
+  /*
+  // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+  // In a storyboard-based application, you will often want to do a little preparation before navigation
+  override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+      // Get the new view controller using segue.destinationViewController.
+      // Pass the selected object to the new view controller.
+  }
+  */
 
 }
